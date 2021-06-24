c27a4f1d128e60172339b5b3fbd502c2e667391a
Manuel <manuelxp27@yahoo.es>
2021-06-24 20:12:08 -0300

    prueba

--- app/genWords.js
+++ app/genWords.js
@@ -2,7 +2,7 @@
 const array = ["Peron", "Mi", "General", "Que", "Grande", "Sos", "Peso", "155", "No", "Como", "Mayonesa", "Con", "Ajo", ];
 
 
-//Randomizador
+//randomizador
 var num = Math.floor(Math.random() * array.length);
 
 console.log(array[num]);
