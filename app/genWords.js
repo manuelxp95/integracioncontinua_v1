408d904f13e471c908f9225528c791e0b5c767ac
Manuel <manuelxp27@yahoo.es>
2021-06-24 19:52:40 -0300

    Agregue todo y travis

---
+++ app/genWords.js
@@ -0,0 +1,8 @@
+//Diccionario
+const array = ["Peron", "Mi", "General", "Que", "Grande", "Sos", "Peso", "155", "No", "Como", "Mayonesa", "Con", "Ajo", ];
+
+
+//Randomizador
+var num = Math.floor(Math.random() * array.length);
+
+console.log(array[num]);
