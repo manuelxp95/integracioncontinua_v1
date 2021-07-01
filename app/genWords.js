857257aa4a05e62cbcda842814b99d486231b8ed
Manuel <manuelxp27@yahoo.es>
2021-07-01 12:04:58 -0300

    prueba exitosa

--- app/genWords.js
+++ app/genWords.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 //Diccionario
-const array = ["Economia", "Mi", "General", "Que", "Grande", "Sos", "Peso", "155", "No", "Como", "Mayonesa", "Con", "Ajo", "お兄ちゃん", "Reddit", "Fluor", "Boca", "River", "Brasil", "Ciruela", "Negocio", "Equipaje", "Microbio", "Goku", "Goya", "Resistencia", "UTN", "Zurdo", "Bicho", "Submarino"];
+const array = ["Economia", "Violeta", "Mi", "General", "Que", "Grande", "Sos", "Peso", "155", "No", "Como", "Mayonesa", "Con", "Ajo", "お兄ちゃん", "Reddit", "Fluor", "Boca", "River", "Brasil", "Ciruela", "Negocio", "Equipaje", "Microbio", "Goku", "Goya", "Resistencia", "UTN", "Zurdo", "Bicho", "Submarino"];
 
 //funcion de aplicacion
 
@@ -46,4 +46,4 @@ app.listen(PORT, function() {
 module.exports = {
     palRandom,
     array
-}
+};
--- test/test.js
+++ test/test.js
@@ -4,13 +4,13 @@ var expect = require("chai").expect;
 
 //var genWords = require("../app/genWords"); //dudoso
 
-const arraytest = ["Economia", "Mi", "General", "Que", "Grande", "Sos", "Peso", "155", "No", "Como", "Mayonesa", "Con", "Ajo", "お兄ちゃん", "Reddit", "Fluor", "Boca", "River", "Brasil", "Ciruela", "Negocio", "Equipaje", "Microbio", "Goku", "Goya", "Resistencia", "UTN", "Zurdo", "Bicho", "Submarino"];
+const arraytest = ["Economia", "Violeta", "Mi", "General", "Que", "Grande", "Sos", "Peso", "155", "No", "Como", "Mayonesa", "Con", "Ajo", "お兄ちゃん", "Reddit", "Fluor", "Boca", "River", "Brasil", "Ciruela", "Negocio", "Equipaje", "Microbio", "Goku", "Goya", "Resistencia", "UTN", "Zurdo", "Bicho", "Submarino"];
 
 describe("Palabras Random - Test", function() {
 
     it("Testing the words equal to the library", function() {
 
-        expect(arraytest).to.have.lengthOf(10);
+        expect(arraytest).to.have.lengthOf(31);
 
     });
 
